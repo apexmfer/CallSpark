1b928812df5e1453a53e4c67f7b0f284427dd0cd
admazzola <admazzola@gmail.com>
2014-12-10 10:16:26 -0500

    Added colors and fixed some profiles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  
+  
+  
+  def color (index)
+    case index
+     when 1
+       return '#69D2E7'
+     when 2
+       return '#A7DBD8'
+     when 3
+        return '#E0E4CC'
+     when 4
+        return '#F38630'
+     else
+        return '#F38630'
+     end
+  end
+  
+  
+  
+  
+  
+  
+  
 end
