21e70088338c56d387b1954c66ec35f08d9a7280
admazzola <admazzola@gmail.com>
2014-12-10 12:05:20 -0500

    Fixed library loading error with datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,8 @@
 //= require jquery.timeago
 //= require jquery-ui.min
 //= require bootstrap.min
-//= require plugins/dataTables/dataTables.bootstrap
 //= require plugins/dataTables/jquery.dataTables
+//= require plugins/dataTables/dataTables.bootstrap
+
 
 
