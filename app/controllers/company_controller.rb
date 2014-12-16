990ae9409ece784d2c5355f9f985b480c0fa304a
admazzola <admazzola@gmail.com>
2014-12-16 14:04:36 -0500

    Updated editing of calls and added better buttons

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,2 +1,15 @@
 class CompanyController < ApplicationController
+  
+  
+  
+  def destroy
+      Company.find(params["id"]).destroy
+  
+
+      redirect_to '/company', alert: 'Company destroyed'
+    
+  end
+  
+  
+  
 end
