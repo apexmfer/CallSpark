b1ef089ebf006a5ff4646baa7a93f815a660cc09
Andy <admazzola@gmail.com>
2015-01-20 12:35:47 -0500

    Companies can now be updated!

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,6 +1,23 @@
 class CompanyController < ApplicationController
   
-  
+    def update
+        name = params['company']['name']
+        bpid = params['company']['BPID']
+      
+      
+      company = Company.find(params['id'])
+      matchingCompany = Company.where(:name => name).first
+      
+      if(matchingCompany == nil)
+        company.name = name;
+        company.BPID = bpid;      
+        company.save
+        redirect_to company, notice: 'Company updated'
+      else
+         redirect_to '/company', alert: 'A company already exists with that name!'
+      end
+      
+    end
   
   def destroy
       Company.find(params["id"]).destroy
