036385dcb78c61806c42b487a07f5fc273aefa21
Arty <arty@arty-VirtualBox.(none)>
2015-02-10 13:04:43 -0500

    shows company name if last name is missing when making a new call

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,24 +1,32 @@
 class Customer < ActiveRecord::Base
    attr_accessible :name, :company_id, :phone_number, :email
-   
+
     def company
-      
+
       return Company.where(id: company_id).first
-  
-      
-  
+
+
+
     end
-    
-    
+
+
     def noLastName
-      
+
       if name.strip.split(" ").length <= 1
           return true
       end
-      
+
       return false
-      
+
     end
-    
-   
+
+    def getAutoCompleteName
+      if noLastName
+          return name.to_s + " @ " + company.name.to_s
+      end
+
+      return name
+
+    end
+
 end
