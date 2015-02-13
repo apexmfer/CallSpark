ac5ba033869ec2b4bcc934f11720100256b97b14
railsTesting <railstesting@railstesting-virtual-machine.(none)>
2015-02-13 16:19:05 -0500

    phone numbers are stored plain now

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,62 +1,62 @@
 class CustomerController < ApplicationController
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
   def create
-    
+
     @customer = spawnCustomer( params[:customer])
-    
+
     redirect_to @customer
   end
-  
-  
-  
+
+
+
   def update
-    
+
     name = params['customer']['name']
     companyname = format_as_company_name(params['customer']['companyname'])
     strippedphone = params['customer']['phone_number'].gsub(/\D/, '')
-    phone = number_to_phone( strippedphone,   area_code: (strippedphone.length > 9))  #strips all but numbers from the input and then formats as phone number
-   email = params['customer']['email'].humanize.delete(' ') 
+    phone = strippedphone  #strips all but numbers from the input and then formats as phone number
+   email = params['customer']['email'].humanize.delete(' ')
     notes = sentencify(params['customer']['notes'])
-    
+
     company = Company.where(name: companyname).first
     companyMatchNil = (company == nil)
-    
+
     if companyMatchNil and companyname.length >= 1
        company = Company.new(name: companyname)
-       company.save  
+       company.save
     end
-    
-    
+
+
     customer = Customer.find(params['id'])
     customer.name = name
     customer.phone_number = phone
     customer.email = email
-    
+
     if(company != nil)
-    customer.company_id = company.id    
+    customer.company_id = company.id
     end
-    
-    customer.notes = notes    
+
+    customer.notes = notes
     customer.save
-    
+
     redirect_to customer
   end
-  
+
    def destroy
     #render text: params
     Customer.find(params["id"]).destroy
 
 
      redirect_to '/customer', alert: 'Customer destroyed'
-  
+
   end
-  
-  
-  
-  
+
+
+
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ActiveRecord::Base
    attr_accessible :name, :company_id, :phone_number, :email
+   include ActionView::Helpers::NumberHelper
+
 
     def company
 
@@ -27,6 +29,22 @@ class Customer < ActiveRecord::Base
 
       return name
 
+    end
+
+    def getFormattedPhone
+      strippedphone = phone_number.gsub(/\D/, '')
+
+      local_number = nil;
+      ext = nil;
+
+      if strippedphone.length <= 10
+        local_number = strippedphone;
+      else
+        local_number = strippedphone[0..9];
+        ext = strippedphone[10..strippedphone.length]
+      end
+      
+      return number_to_phone(local_number ,   area_code: (strippedphone.length > 9), extension: ext)
     end
 
 end
