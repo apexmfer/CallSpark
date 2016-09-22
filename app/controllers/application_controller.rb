aea13d8e9fff4fe36baecda94de6f8dc27579cd1
Andy M <dev.sykophant@gmail.com>
2016-09-22 11:00:54 -0400

    added region

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :require_login
-  
-  
+  #before_filter :require_login
+
+
   private
 def not_authenticated
   redirect_to login_path, alert: "Please login first"
@@ -13,115 +13,115 @@ end
 include ActionView::Helpers::NumberHelper
 
   def sentencify(input)
-    
+
    return input.gsub(/([a-z])((?:[^.?!]|\.(?=[a-z]))*)/i) { $1.upcase + $2.rstrip }
-    
+
   end
-  
+
   def capFirstLetter(word)
-    
+
     word[0] = word[0].upcase
-    
+
     return word
-    
+
   end
-  
-  
+
+
   def format_as_company_name(input)
-    
+
     return input.split(" ").map{|word| capFirstLetter(word)}.join(" ")
-    
+
   end
 
 
   #This creates a new customer with a form and does special checks to make a new company
   def spawnCustomer(params )
-    
-    caller = (params[:caller]).titleize 
-    companyname = format_as_company_name(params[:company]) 
-    
+
+    caller = (params[:caller]).titleize
+    companyname = format_as_company_name(params[:company])
+
     strippedphone = params[:phone].gsub(/\D/, '')
     phone = number_to_phone( strippedphone,   area_code: (strippedphone.length > 9))  #strips all but numbers from the input and then formats as phone number
-    email = (params[:email]).humanize.delete(' ') 
-    
-    
+    email = (params[:email]).humanize.delete(' ')
+
+
     #Try to find existing matches first
     customer = Customer.where(name: caller).first
-    
+
     company = Company.where(name: companyname).first
-    
-    
+
+
     #If there is no company match, store this info and make a brand new company record
     companyMatchNil = (company == nil)
-    
+
       if companyMatchNil and companyname.length >= 1
-        
-        
+
+
         company = Company.new(:name => companyname,
         :BPID => params[:BPID]
         )
-        
+
            company.save
-        
-         
+
+
       end
-      
-     
-    
+
+
+
     #If the customer is brand new or if (the company is new and the customer has no last name) then make a new customer
     #This prevents overwriting other existing customers when somebody is added with no last name
     if customer == nil or (customer.noLastName and companyMatchNil)
-      
+
      company_id = nil
       if company
         company_id = company.id
       end
-            
+
       customer = Customer.new(:name => caller,
      :company_id => company_id,
       :phone_number => phone,
       :email => email
       )
-      
+
     else
-    
+
       #Update the existing customer info
       if( company != nil )
        customer.company_id =  company.id;
       end
-    
+
      customer.phone_number =  phone;
      customer.email = email;
-      
+
     end
-    
+
     #save our updates
-    customer.save 
-    
+    customer.save
+
         if( company != nil )
         company.save
          end
-         
-         
-         
-         
-    
-    
+
+
+
+
+
+
     return customer
   end
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
--- app/controllers/region_controller.rb
+++ app/controllers/region_controller.rb
@@ -1,2 +1,28 @@
 class RegionController < ApplicationController
+
+
+  def create
+
+    @region = Region.new(new_region_params)
+
+    @region.save!
+
+    redirect_to '/admin/regions', alert: 'Region created'
+  end
+
+  def destroy
+
+    Region.find(params["regions_id"]).destroy!
+
+      redirect_to '/admin/regions', alert: 'Region destroyed'
+
+  end
+
+private
+
+def new_region_params
+    params.require(:region).permit(:name )
+end
+
+
 end
