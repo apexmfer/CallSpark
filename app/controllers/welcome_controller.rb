108ce477f97587bfe025ec5aed3422e378670ed3
Andy M <dev.sykophant@gmail.com>
2016-10-12 17:07:27 -0400

    fixed dates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,21 +10,24 @@ class WelcomeController < ApplicationController
 
         @calls_overview_labels = []
 
-      today= Time.now.end_of_day
+      end_of_today = Time.now.end_of_day
+
       daysToSee = 30
 
       daysToSee.times do
 
-      numcalls = Call.where(created_at: (today - 1.day)..today).length
+        beginning_of_today = (end_of_today - 1.day)
 
-        today = today - 1.day
+      numcalls = Call.where(created_at: beginning_of_today..end_of_today).length
 
-        datetime_unix = ((today - 12.hours).to_i )
+        datetime_unix = ((beginning_of_today + 6.hours).to_i )
 
         datetime = Time.at(datetime_unix)
 
       @calls_overview_data.push numcalls
       @calls_overview_labels.push datetime.strftime("%b %d")
+
+      end_of_today = end_of_today - 1.day
 
      end
 
